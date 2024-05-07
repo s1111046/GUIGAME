cfdee439ebe45316d283843fac3cf45c149e556f
s1111046 <ykc4957@gmail.com>
2024-05-08 00:09:53 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private XaXbEngine xaxb;
+        private int counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            xaxb = new XaXbEngine();
+            counter = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string userNum = textBox2.Text.Trim(); // 获取用户输入的数字
+
+            if (!xaxb.IsLegal(userNum)) // 如果用户输入不合法
+            {
+                MessageBox.Show("請輸入的資料重複，或長度不對！！請重新輸入！！");
+                return; // 不执行下面的代码，直接返回
+            }
+
+            if (xaxb.IsGameover(userNum))
+            {
+                MessageBox.Show("恭喜你猜對了！");
+            }
+            else
+            {
+                counter++;
+                string result = xaxb.GetResult(userNumber: userNum);
+                listBox1.Items.Add($"{userNum} : {result},猜測次數: {counter}");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/XaXbEngine .cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    internal class XaXbEngine
+    {
+        // 幸运数字
+        private string luckyNumber;
+
+        // 构造函数，初始化幸运数字
+        public XaXbEngine()
+        {
+            // 生成随机数对象
+            Random random = new Random();
+            int[] tem = new int[3];
+            tem[0] = random.Next(0, 9);
+            tem[1] = random.Next(0, 9);
+            tem[2] = random.Next(0, 9);
+
+            // 确保生成的幸运数字没有重复的数字
+            while (tem[0] == tem[1] || tem[1] == tem[2] || tem[0] == tem[2])
+            {
+                tem[1] = random.Next(0, 9);
+                tem[2] = random.Next(0, 9);
+            }
+
+            // 将生成的数字组合成字符串作为幸运数字
+            luckyNumber = $"{tem[0]}{tem[1]}{tem[2]}";
+        }
+
+        // 判断用户输入的数字是否合法（三个数字不相同）
+        public bool IsLegal(string theNumber)
+        {
+            char[] tem = theNumber.ToCharArray();
+            if (tem.Length == 3)
+            {
+                if (tem[0] != tem[1] && tem[1] != tem[2] && tem[0] != tem[2])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // 根据用户输入的数字计算结果
+        public string GetResult(string userNumber)
+        {
+            char[] user = userNumber.ToCharArray();
+            char[] ans = this.luckyNumber.ToCharArray();
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < user.Length; i++)
+            {
+                for (int j = 0; j < ans.Length; j++)
+                {
+                    if (user[i] == ans[j])
+                    {
+                        if (i == j)
+                        {
+                            a++;
+                        }
+                        else
+                        {
+                            b++;
+                        }
+                    }
+                }
+            }
+            return $"{a}A{b}B";
+        }
+
+        // 判断是否猜中幸运数字
+        public bool IsGameover(string userNumber)
+        {
+            return GetResult(userNumber) == "3A0B";
+        }
+    }
+}
